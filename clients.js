52029eb43f50b6527c7257a1d35d7059763efed6
Brian Ladner <github@theladners.com>
2015-08-06 09:27:48 -0500

    added functionality

--- clients.js
+++ clients.js
@@ -12,7 +12,34 @@ function clients() {
         // Controls for all Garage Door functions
         controls: {
             refresh: {label: 'Refresh', action: 'STATUS', icon: 'refresh'},
-            change: {label: 'Change', action: 'CHANGE', icon: 'arrow-u', css: 'background-color:#FF9B9B;'}
+            change: {label: 'Change', action: 'CHANGE', icon: 'arrow-u', css: 'background-color:#FF9B9B;'},
+            photo: {label: 'Photo', action: 'PHOTO', icon: null}
+        }
+    };
+    this.sprinkler = {
+        label: 'Sprinkler System',
+        icon: null,
+        // Available triggers to send alerts
+        triggers: {
+            watering: {label: 'Watering'}
+        },
+        // Controls for all Garage Door functions
+        controls: {
+            refresh: {label: 'Refresh', action: 'STATUS', icon: 'refresh'},
+            run: {label: 'Run', action: 'RUN', icon: 'arrow-u', css: 'background-color:#FF9B9B;'}
+        }
+    };
+    this.alarm = {
+        label: 'Alarm System',
+        icon: null,
+        // Available triggers to send alerts
+        triggers: {
+            alarm: {label: 'Alarm'}
+        },
+        // Controls for all Garage Door functions
+        controls: {
+            refresh: {label: 'Refresh', action: 'STATUS', icon: 'refresh'},
+            arm: {label: 'Arm', action: 'ARM', icon: 'arrow-u', css: 'background-color:#FF9B9B;'}
         }
     };
 }
